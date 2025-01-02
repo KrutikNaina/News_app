6501a719935ea6000f5cafcef3d6fe8bfc444253
Krutik Naina <nenakrutik2004@gmail.com>
2025-01-02 21:49:36 +0530

    final commit

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,22 +1,31 @@
 import React from 'react'
 
 const Card = ({data}) => {
-    console.log(data);
+     console.log(data);
 
+     const readMore = (url) =>{
+        window.open(url)
+     }
+     
   return (
     <div className='cardContainer'>
-        {data.map((curItem,index)=>{
+    {data.map((curItem,index)=>{
+        if(!curItem.urlToImage){
+            return null
+        }else{
             return(
-                <div className='card'>
-                    <img src={curItem.urlToImage}/>
-                    <div className='content'>
-                        <a className='title'>{curItem.title}</a>
-                        <p>{curItem.description}</p>
-                        <button onClick={()=>curItem.url}>Read More</button>
-                    </div>
+            <div className='card'>
+                <img src={curItem.urlToImage}/>
+                <div className='content'>
+                    <a className='title' onClick={()=>window.open(curItem.url)}>{curItem.title}</a>
+                    <p>{curItem.description}</p>
+                    <button onClick={()=>window.open(curItem.url)}>Read More</button>
                 </div>
-            )
-        })}
+            </div>
+        )
+        }
+         
+    })}
     </div>
   )
 }
--- src/Components/Newsapp.js
+++ src/Components/Newsapp.js
@@ -1,56 +1,63 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from './Card'
 
-function Newsapp() {
-
-    const [search, setSearch] = useState("india")
+const Newsapp = () => {
+    const [search, setSearch] = useState("india");
     const [newsData, setNewsData] = useState(null)
-    const API_KEY = "a51a77d8126f4e47a38a53a49e86a277";
+    const API_KEY = "9c3ed8ee95884dec979460a60f96675b";
 
     const getData = async() =>{
-        const response = await fetch(`https://newsapi.org/v2/everything?q=${search}&apikey=${API_KEY}`)
-        const jsonData = await response.json()
+        const response = await fetch(`https://newsapi.org/v2/everything?q=${search}&apiKey=${API_KEY}`);
+        const jsonData = await response.json();
         console.log(jsonData.articles);
-        setNewsData(jsonData.articles)
+        let dt = jsonData.articles.slice(0,10)
+        setNewsData(dt)
+        
     }
+
+    useEffect(()=>{
+        getData()
+    },[])
 
     const handleInput = (e) =>{
         console.log(e.target.value);
         setSearch(e.target.value)
-
+        
+    }
+    const userInput = (event) =>{
+        setSearch(event.target.value)
     }
 
   return (
     <div>
         <nav>
             <div>
-                <h1>Trending News</h1>
+                <h1>Trendy News</h1>
             </div>
-            <ul>
-                <a>All News</a>
-                <a>Trending</a>
+            <ul style={{display:"flex", gap:"11px"}}>
+                <a style={{fontWeight:600, fontSize:"17px"}}>All News</a>
+                <a style={{fontWeight:600, fontSize:"17px"}}>Trending</a>
+
             </ul>
             <div className='searchBar'>
-                <input type="text" placeholder="Search News" onChange={handleInput}/>
+                <input type='text' placeholder='Search News' value={search} onChange={handleInput}/>
                 <button onClick={getData}>Search</button>
             </div>
         </nav>
-
         <div>
-            <p className='head'>Stay Update with Trending News</p>
+            <p className='head'>Stay Update with TrendyNews</p>
         </div>
-
         <div className='categoryBtn'>
-            <button>Business</button>
-            <button>Entertainment</button>
-            <button>Sports</button>
-            <button>Politics</button>
-            <button>Technology</button>
-            <button>Health</button>
+            <button onClick={userInput} value="sports">Sports</button>
+            <button onClick={userInput} value="politics">Politics</button>
+            <button onClick={userInput} value="entertainment">Entertainment</button>
+            <button onClick={userInput} value="health">Health</button>
+            <button onClick={userInput} value="fitness">Fitness</button>
         </div>
 
         <div>
-            {newsData? <Card data={newsData}/> : null}
+        {newsData?  <Card data={newsData}/> : null}
+            
         </div>
     </div>
   )
